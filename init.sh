67779d5d1be6e6d223be59e9f8c22ee8d0ac613d
Meng Jie <inmouse@gmail.com>
2014-12-20 19:04:10 +0800

    加入spf13的个性化配置, 以及一些不能直接配置地方的说明

--- init.sh
+++ init.sh
@@ -23,3 +23,14 @@ ln -s $REPOPATH/.tmux.conf ~/.tmux.conf
 rm -rf ~/.termcap
 ln -s $REPOPATH/.termcap ~/.termcap
 
+# 对spf13还有这几个调整
+# 1. .vimrc. ctrlp unlet 变量 g:ctrlp_user_command
+# 2. .vimrc. ctrlp 可以加入几个过滤的条件.
+# 3. .vimrc.bundles 注释掉 vim-indent-guides
+
+rm -rf ~/.vimrc.local
+ln -s $REPOPATH/.vimrc.local ~/.vimrc.local
+
+rm -rf ~/.vimrc.before.local
+ln -s $REPOPATH/.vimrc.before.local ~/.vimrc.before.local
+
